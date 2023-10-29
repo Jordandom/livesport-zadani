4cdac12a765cd1d6034f76585683a2c185e6a58e
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-10-29 20:36:11 +0100

    added comments for fns

--- cypress/component/results.cy.tsx
+++ cypress/component/results.cy.tsx
@@ -3,6 +3,7 @@ import { mount } from 'cypress/react18';
 import TestResultsComponent from '../../src/components/test-results';
 import { QueryClientProvider } from '../../src/utils/data-fetching/react-query';
 import mockedData from '../fixtures/mocked-results-data.json';
+
 const URL =
   'https://s.livesport.services/api/v2/search?lang-id=1&project-id=602&project-type-id=1&sport-ids=1,2,3,4,5,6,7,8,9&type-ids=1%2C2%2C3%2C4&q=aa';
 
