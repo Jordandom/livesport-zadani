480f0607698cfba272ec75ef5929ad7091707a80
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-10-29 20:58:35 +0100

    fixed

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,4 +1,4 @@
-import Input from '@components/input';
+import Input from '@components/inpu';
 import Button from '@components/button';
 import { useFilterActions } from '@store/filter-store';
 import { useState } from 'react';
