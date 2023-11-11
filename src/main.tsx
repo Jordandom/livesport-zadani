e6984787d7722845446a8926c62136c2c48ce8bc
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-11-11 12:00:22 +0100

    fixed bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import './index.css';
 
+import App from 'ap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
-import App from './app';
 import { QueryClientProvider } from './utils/data-fetching/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
